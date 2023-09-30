da628d3c54f3aa3745f72b6b978c95a4674e525a
James Ross <james.i.ross5@gmail.com>
2023-09-30 21:41:56 +0100

    Load today's stats immediately

--- backend/app/views.py
+++ backend/app/views.py
@@ -35,8 +35,8 @@ def home_view(request):
             "sorted_todos": sorted_todos,
             "unsorted_todos": unsorted_todos,
             "new_categories": categories,
-            "number_todos_created": models.Todo.objects.created_today().count(),
-            "number_todos_today": models.Todo.objects.completed_today().count(),
+            # "number_todos_created": models.Todo.objects.created_today().count(),
+            # "number_todos_today": models.Todo.objects.completed_today().count(),
             "form": forms.NewTodoForm(),
         },
     )
